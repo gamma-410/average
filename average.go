217feffe89c9f4da496fffac2b7c3e507269ad05
gamma-410 <mail@gamma410.win>
2022-08-03 01:08:33 +0900

    git init && go build

---
+++ average.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "fmt"
+  "flag"
+)
+
+var nationalLang int = 63
+var socialStudies int = 50
+var science int = 65
+
+func main() {
+  flag.Parse()
+  arg := flag.Arg(0)
+
+  switch arg {
+    case "total":
+      total()
+    case "average":
+      average()
+    default:
+      fmt.Println("Please specify an argument.")
+  }
+}
+
+func total() {
+  total := nationalLang + socialStudies + science
+  fmt.Println(total)
+}
+
+func average() {
+  total := nationalLang + socialStudies + science
+  average := total / 3
+  fmt.Println(average) 
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module gamma410.win/average
+
+go 1.18
