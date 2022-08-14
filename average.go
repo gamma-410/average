bc5e63d6f6d0f9e0b4889b62e222828c263f61c9
gamma-410 <mail@gamma410.win>
2022-08-14 09:02:17 +0900

    fmt

--- average.go
+++ average.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-  "fmt"
-  "flag"
+	"flag"
+	"fmt"
 )
 
 var nationalLang int = 63
@@ -10,27 +10,26 @@ var socialStudies int = 50
 var science int = 65
 
 func main() {
-  flag.Parse()
-  arg := flag.Arg(0)
+	flag.Parse()
+	arg := flag.Arg(0)
 
-  switch arg {
-    case "total":
-      total()
-    case "average":
-      average()
-    default:
-      fmt.Println("Please specify an argument.")
-  }
+	switch arg {
+	case "total":
+		total()
+	case "average":
+		average()
+	default:
+		fmt.Println("Please specify an argument.")
+	}
 }
 
 func total() {
-  total := nationalLang + socialStudies + science
-  fmt.Println(total)
+	total := nationalLang + socialStudies + science
+	fmt.Println(total)
 }
 
 func average() {
-  total := nationalLang + socialStudies + science
-  average := total / 3
-  fmt.Println(average) 
+	total := nationalLang + socialStudies + science
+	average := total / 3
+	fmt.Println(average)
 }
-
